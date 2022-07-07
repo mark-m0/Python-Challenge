29a7d5636e478d93beb1473dd638ff3a2d0f8616
Mark Martinez <mark.martinez8230@gmail.com>
2022-07-07 16:40:11 -0500

    counter for lines added

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,4 +6,7 @@ csvpath = os.path.join('Resources', 'budget_data.csv')
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
+#count all of the lines in the file, then subtract 1 for the header
+    lines = len(list(csvreader))
+    print((lines-1))
 
