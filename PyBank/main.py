4f4d78432042f46e43c1fdc20bf341233ab3cf98
Mark Martinez <mark.martinez8230@gmail.com>
2022-07-07 16:14:10 -0500

    edit to reader

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,9 @@
 import os
 import csv
 
-csvpath = os.path.join("..", "Resources", "budget_data.csv")
+csvpath = os.path.join('Resources', 'budget_data.csv')
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
+
+
