a1d67cf14b0c69099c4bfbeb062db8ba61779ad4
Mark Martinez <mark.martinez8230@gmail.com>
2022-07-11 15:58:32 -0500

    PyPoll started, PyBank edited for attempt at sum of col2

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,21 +5,20 @@ csvpath = os.path.join('Resources', 'budget_data.csv')
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
+    
 #skip the header when using any values moving forward
-    header = next(csvreader)
-
+    for row in csvreader:
 # Count all of the lines in the file
-    lines = len(list(csvreader))
-
+        total = 0
+        lines = len(list(csvreader))
+        total = row[1]
 # Add the values of the 2nd line (profit/losses) skipping the header
-      
-
-      
+    
 # Print out the financial analysis
-   # print("Financial Analysis:")
-    #print("-----------------------------")
-    #print(f"Total Months: {lines}")
-    #print(f"Total: {total}")
+    print("Financial Analysis:")
+    print("-----------------------------")
+    print(f"Total Months: {lines}")
+    print(f"Total: {total}")
     #print(f"Average Change: {avgChange}")
     #print(f"Greatest Increase in Profits: {greatIncr}")
     #print(f"Greatest Decrease in Profits: {greatDecr}")
---
+++ PyPoll/main.py
@@ -0,0 +1,18 @@
+import os
+import csv
+
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    header = next(csvreader)
+    votes = len(list(csvreader))
+
+
+    print("Election Results:")
+    print("-----------------------------")
+    print(f"Total Votes: {votes}")
+    #print(f"Total: {total}")
+    #print(f"Average Change: {avgChange}")
+    #print(f"Greatest Increase in Profits: {greatIncr}")
+    #print(f"Greatest Decrease in Profits: {greatDecr}")
